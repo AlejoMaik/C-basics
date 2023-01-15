02d55d8129cbc8fa02ffb95e0b59b89c418d710a
AlejoMaik <labaenam@eafit.edu.co>
2023-01-15 18:44:41 -0500

    Typo

---
+++ Bleep/fBleep.h
@@ -0,0 +1,7 @@
+#ifndef BEGINNING_FBLEEP_H
+#define BEGINNING_FBLEEP_H
+
+void bleep(std::string word, std::string &text); //Function to write a "bleep" instead of a selected word.
+
+
+#endif //BEGINNING_FBLEEP_H
