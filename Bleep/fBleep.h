249b7a11f5f8ee59fa33f78ea17c60292ba901ac
AlejoMaik <labaenam@eafit.edu.co>
2023-01-17 10:03:29 -0500

    Header file of Bleep program

--- Bleep/fBleep.h
+++ Bleep/fBleep.h
@@ -3,7 +3,7 @@
 #ifndef BEGINNING_FBLEEP_H
 #define BEGINNING_FBLEEP_H
 
-void bleep(std::string word, std::string &text);
+void bleep(std::string word, std::string &text); //Definition of bleep function
 
 
 #endif //BEGINNING_FBLEEP_H
