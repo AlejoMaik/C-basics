c8e3aba597ca97d3be96430ab96c1f58e81170cd
AlejoMaik <labaenam@eafit.edu.co>
2023-01-18 11:10:45 -0500

    Functions of Bleep program

--- Bleep/fBleep.cpp
+++ Bleep/fBleep.cpp
@@ -3,16 +3,16 @@
 void bleep(std::string word, std::string &text) {
 
     for (int i = 0; i < text.size(); i++) {
-        int match = 0;
-        for (int j = 0; j < word.size(); j++) {
+        int match = 0; //number of letter equal to the word
+        for (int j = 0; j < word.size(); j++) { //Nested loop to see if there is a match
             if (text[i+j] == word[j]) {
-                match++;
+                match++; //number of letter equal to the word
             }
         }
 
-        if (match == word.size()) {
-            for (int j = 0; j < word.size(); j++) {
-                text[i+j] = '*';
+        if (match == word.size()) { //If all the letters match, then...
+            for (int j = 0; j < word.size(); j++) { //Let's bleep those letter
+                text[i+j] = '*'; //We change the word for *
             }
         }
     }
